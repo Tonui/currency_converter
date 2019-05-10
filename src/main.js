6b834a5d11cee4bda20741e19f89f37d658ee757
tonui <tktonui@outlook.com>
2019-05-10 11:20:51 +0300

    tried resolving select btn issue

--- src/main.js
+++ src/main.js
@@ -30,13 +30,12 @@ fetch(`https://free.currconv.com/api/v7/countries?apiKey=${SERVER_API}`)
         let currency_namesStore = tx.objectStore('currency_names');
         console.log('Checkpoint: idbThen')
         for (var currency in currencies) {
-            console.log('idb for loop')
             var the_currency_obj = currencies[currency]
             var currency_id = the_currency_obj.currencyId
             var currency_name = the_currency_obj.currencyName
+            from_currency_select.innerHTML += `<option value="${currency_id}">${currency_name}</option>`
+            to_currency_select.innerHTML += `<option value="${currency_id}">${currency_name}</option>`
             currency_namesStore.put(the_currency_obj)
-            from_currency_select.innerHTML += `<option value=${currency_id}>${currency_name}</option>`
-            to_currency_select.innerHTML += `<option value=${currency_id}>${currency_name}</option>`
         }
         return tx.complete
     })

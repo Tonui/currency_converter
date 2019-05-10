05e92edf62247c41dc583b9fd8fcd11b1025b281
tonui <tktonui@outlook.com>
2019-05-10 10:50:29 +0300

    tried resolving select btn issue

--- docs/main.js
+++ docs/main.js
@@ -18,13 +18,17 @@ fetch('https://free.currconv.com/api/v7/countries?apiKey=' + SERVER_API).then(fu
     var from_currency_select = document.getElementById('from_currency');
     var to_currency_select = document.getElementById('to_currency');
     var parsedData = data.json();
+    console.log('Parsed data: ', parsedData);
     var currencies = parsedData.results;
+    console.log('Currencies: ', currencies);
 
     // Save currencies to idb
     dbPromise.then(function (db) {
         var tx = db.transaction('currency_names', 'readwrite');
         var currency_namesStore = tx.objectStore('currency_names');
+        console.log('Checkpoint: idbThen');
         for (var currency in currencies) {
+            console.log('idb for loop');
             var the_currency_obj = currencies[currency];
             var currency_id = the_currency_obj.currencyId;
             var currency_name = the_currency_obj.currencyName;
--- src/main.js
+++ src/main.js
@@ -17,13 +17,17 @@ fetch(`https://free.currconv.com/api/v7/countries?apiKey=${SERVER_API}`)
     var from_currency_select = document.getElementById('from_currency')
     var to_currency_select = document.getElementById('to_currency')
     var parsedData = data.json()
+    console.log('Parsed data: ', parsedData)
     var currencies = parsedData.results
+    console.log('Currencies: ', currencies)
 
     // Save currencies to idb
     dbPromise.then(db => {
         let tx = db.transaction('currency_names', 'readwrite')
         let currency_namesStore = tx.objectStore('currency_names');
+        console.log('Checkpoint: idbThen')
         for (var currency in currencies) {
+            console.log('idb for loop')
             var the_currency_obj = currencies[currency]
             var currency_id = the_currency_obj.currencyId
             var currency_name = the_currency_obj.currencyName
